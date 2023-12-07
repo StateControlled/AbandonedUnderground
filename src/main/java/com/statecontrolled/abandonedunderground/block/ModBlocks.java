e671017c298ec9031a373e1e571be24e81117d76
William Berthouex <william.berthouex@gmail.com>
2023-12-07 09:10:32 -0600

    AbandonedUnderground Initial
    Setup

---
+++ src/main/java/com/statecontrolled/abandonedunderground/block/ModBlocks.java
@@ -0,0 +1,104 @@
+package com.statecontrolled.abandonedunderground.block;
+
+import java.util.function.Supplier;
+
+import com.statecontrolled.abandonedunderground.AbandonedUnderground;
+import com.statecontrolled.abandonedunderground.item.ModItems;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, AbandonedUnderground.MOD_ID);
+
+    public static final RegistryObject<Block> SMALL_WHITE_TILES = registerBlock("small_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CRACKED_SMALL_WHITE_TILES = registerBlock("cracked_small_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DIRTY_SMALL_WHITE_TILES = registerBlock("dirty_small_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MOSSY_SMALL_WHITE_TILES = registerBlock("mossy_small_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CHISELED_SMALL_WHITE_TILE = registerBlock("chiseled_small_white_tile",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> WHITE_TILES = registerBlock("white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CRACKED_WHITE_TILES = registerBlock("cracked_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DIRTY_WHITE_TILES = registerBlock("dirty_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MOSSY_WHITE_TILES = registerBlock("mossy_white_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CHISELED_WHITE_TILE = registerBlock("chiseled_white_tile",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> SMALL_BLACK_TILES = registerBlock("small_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CRACKED_SMALL_BLACK_TILES = registerBlock("cracked_small_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DIRTY_SMALL_BLACK_TILES = registerBlock("dirty_small_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MOSSY_SMALL_BLACK_TILES = registerBlock("mossy_small_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CHISELED_SMALL_BLACK_TILE = registerBlock("chiseled_small_black_tile",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> BLACK_TILES = registerBlock("black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CRACKED_BLACK_TILES = registerBlock("cracked_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DIRTY_BLACK_TILES = registerBlock("dirty_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MOSSY_BLACK_TILES = registerBlock("mossy_black_tiles",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CHISELED_BLACK_TILE = registerBlock("chiseled_black_tile",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> CONCRETE_PLATFORM = registerBlock("concrete_platform",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> CRACKED_CONCRETE_PLATFORM = registerBlock("cracked_concrete_platform",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DIRTY_CONCRETE_PLATFORM = registerBlock("dirty_concrete_platform",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MOSSY_CONCRETE_PLATFORM = registerBlock("mossy_concrete_platform",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> LINED_CONCRETE_PLATFORM = registerBlock("lined_concrete_platform",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> SMOOTH_LIGHT = registerBlock("smooth_light",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> METAL_GRATE_FLOOR = registerBlock("metal_grate_floor",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> STEEL_FLOOR_PLATE = registerBlock("steel_floor_plate",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> SMOOTH_METAL_FLOOR = registerBlock("smooth_metal_floor",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).requiresCorrectToolForDrops()));
+
+    private ModBlocks() {
+        ;
+    }
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> result = BLOCKS.register(name, block);
+        registerBlockItem(name, result);
+        return result;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/com/statecontrolled/abandonedunderground/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.statecontrolled.abandonedunderground.item;
+
+import com.statecontrolled.abandonedunderground.AbandonedUnderground;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, AbandonedUnderground.MOD_ID);
+
+//    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+//            () -> new Item(new Item.Properties()));
+
+    private ModItems() {
+        ;
+    }
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
